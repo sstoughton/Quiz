3aa584fa362237fd481f672fa7273318096cc904
Savannah Stoughton <sstoughton@vision.local>
2019-01-27 11:55:27 -0500

    Draft 1 of first Quiz ios App

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  Quiz
-//
+//  CSC 2310
 //  Created by Savannah Stoughton on 1/23/19.
 //  Copyright © 2019 Savannah Stoughton. All rights reserved.
 //
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    //questions stored as constant strings...value can't be changed
+    let questions: [String] = [
+       "What is 7+7?",
+       "What is the capital of Vermont?",
+       "What is cognac made from?"
+    ]
+    
+    //questions stored as constant strings...value can't be changed
+    let answers: [String] = [
+       "14",
+       "Montpelier",
+       "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
 
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    //load question into ??? placeholder
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
