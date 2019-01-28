34a37f672f341096c28feff2559e73d70f9fa29f
Savannah Stoughton <sstoughton@vision.local>
2019-01-28 13:56:13 -0500

    Add P2 updates to app

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -3,6 +3,7 @@
 //  Quiz
 //  CSC 2310
 //  Created by Savannah Stoughton on 1/23/19.
+//  High Point University
 //  Copyright © 2019 Savannah Stoughton. All rights reserved.
 //
 
@@ -16,14 +17,25 @@ class ViewController: UIViewController {
     let questions: [String] = [
        "What is 7+7?",
        "What is the capital of Vermont?",
-       "What is cognac made from?"
+       "What is cognac made from?",
+       "When was High Point University founded?",
+       "What is the High Point University mascot?",
+       "How many Division 1 athletic teams does HPU have?",
+       "How many hours a day does a sloth typically sleep?",
+       "What is the capital of North Carolina?"
+       
     ]
     
     //questions stored as constant strings...value can't be changed
     let answers: [String] = [
        "14",
        "Montpelier",
-       "Grapes"
+       "Grapes",
+       "1924",
+       "Panther",
+       "16",
+       "10 hours",
+       "Raleigh"
     ]
     
     var currentQuestionIndex: Int = 0
